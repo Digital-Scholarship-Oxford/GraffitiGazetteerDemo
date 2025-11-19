ad407a9e4a90ae34e744daac5981e6eb69aa9c6b
Imran Asif <56230659+imranasif87@users.noreply.github.com>
2025-11-19 20:39:09 +0000

    Deploy

--- src/GraffitiGazetteerDemo.tsx
+++ src/GraffitiGazetteerDemo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { ChevronLeft, ChevronRight, Database, Upload, Link, CheckCircle, Users, MapPin, ExternalLink } from 'lucide-react';
 
 const GraffitiGazetteerDemo = () => {
